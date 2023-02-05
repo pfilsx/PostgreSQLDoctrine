27fbf424fe5ff39096d80f3fcad5ff3ac269465b
Pfilsx <pfilsx@gmail.com>
2023-02-05 14:55:30 +0300

    add json basic functions support

---
+++ src/ORM/Query/AST/Functions/JsonGetArrayElement.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pfilsx\PostgreSQLDoctrine\ORM\Query\AST\Functions;
+
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+
+/**
+ * Implementation of PostgreSql json array field retrieval by index
+ * @see https://www.postgresql.org/docs/current/functions-json.html
+ * @example JSON_GET_ARRAY_ELEMENT(entity.field, 1)
+ */
+final class JsonGetArrayElement extends JsonGetField
+{
+    public function parse(Parser $parser): void
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $this->field = $parser->StringPrimary();
+        $parser->match(Lexer::T_COMMA);
+        $this->path[] = $parser->ArithmeticPrimary();
+
+        if (!$parser->getLexer()->isNextToken(Lexer::T_CLOSE_PARENTHESIS)) {
+            while ($parser->getLexer()->isNextToken(Lexer::T_COMMA)) {
+                $parser->match(Lexer::T_COMMA);
+                $this->path[] = $parser->ArithmeticPrimary();
+            }
+        }
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
---
+++ src/ORM/Query/AST/Functions/JsonGetFieldAsText.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pfilsx\PostgreSQLDoctrine\ORM\Query\AST\Functions;
+
+use Doctrine\ORM\Query\AST\Node;
+use Doctrine\ORM\Query\SqlWalker;
+
+/**
+ * Implementation of PostgreSql json object field as text retrieval by path
+ * @see https://www.postgresql.org/docs/current/functions-json.html
+ * @example JSON_GET_FIELD_AS_TEXT(entity.field, 'a', 'b')
+ */
+class JsonGetFieldAsText extends JsonGetField
+{
+    public function getSql(SqlWalker $sqlWalker): string
+    {
+        $path = array_map(static fn (Node $node) => $node->dispatch($sqlWalker), $this->path);
+
+        $lastPathElement = array_pop($path);
+
+        $sql = $this->field->dispatch($sqlWalker);
+
+        if (count($path) > 0) {
+            $sql .= sprintf('->%s', implode('->', $path));
+        }
+
+        return "$sql->>$lastPathElement";
+    }
+}
